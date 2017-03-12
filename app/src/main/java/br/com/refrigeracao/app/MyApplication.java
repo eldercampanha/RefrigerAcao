e083861401b68f2638d33bbcf594c85bf4d2ed2c
Elder Baltazar <Elder Baltazar>
2017-03-12 03:02:01 -0300

    [FIREBASE] binding view and firebase according to the new firebase ui

--- app/src/main/java/br/com/refrigeracao/app/MyApplication.java
+++ app/src/main/java/br/com/refrigeracao/app/MyApplication.java
@@ -2,6 +2,10 @@ package br.com.refrigeracao.app;
 
 import android.app.Application;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
+
 import br.com.refrigeracao.app.dagger.component.DaggerDiComponent;
 import br.com.refrigeracao.app.dagger.component.DiComponent;
 import br.com.refrigeracao.app.dagger.module.ApplicationModule;
@@ -18,9 +22,16 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        // Dagger
         mDiComponent = DaggerDiComponent.builder()
                 .applicationModule(new ApplicationModule(this))
                 .build();
+
+        Firebase.setAndroidContext(this);
+
+        if(!FirebaseApp.getApps(this).isEmpty()){
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        }
     }
 
     public DiComponent getDaggerUiComponent() {
