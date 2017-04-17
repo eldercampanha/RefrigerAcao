1ef4ba824291905b040520fe3c93ab42660fcb6e
Elder Baltazar <Elder Baltazar>
2017-04-16 21:52:13 -0300

    [CREATE-ORDER] creating activity

---
+++ app/src/main/java/br/com/refrigeracao/app/presentation/ui/createorder/CreateOrderActivity.java
@@ -0,0 +1,15 @@
+package br.com.refrigeracao.app.presentation.ui.createorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.refrigeracao.R;
+
+public class CreateOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_order);
+    }
+}
